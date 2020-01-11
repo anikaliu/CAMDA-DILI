cad57d5c0be84e47d1f32d79e5d2fd7510e080b9
mowal <52958013+mowal@users.noreply.github.com>
2020-01-11 19:06:34 +0000

    Update models_ECFP.py

--- Machine_Learning/code/models_ECFP.py
+++ Machine_Learning/code/models_ECFP.py
@@ -344,7 +344,7 @@ df_ts['ROC_AUC'] = rocauc_ts
 df_ts['MCC'] = mcc_ts
 
 
-df_ts.to_csv('CAMDA-DILI/data/processed_data/Models/ECFP/ts_scores_FP_yscr.csv',sep=',',index=False)
+df_ts.to_csv('CAMDA-DILI/data/processed_data/Models/ECFP/test_scores_ECFP.csv',sep=',',index=False)
 
 df_cv = pd.DataFrame()
 df_cv['splits'] = cv_splits
@@ -356,10 +356,10 @@ df_cv['AU_Prec_Rec_Curve'] = aupr
 df_cv['ROC_AUC'] = rocauc
 df_cv['MCC'] = mcc
 
-df_cv.to_csv('CAMDA-DILI/data/processed_data/Models/ECFP/cv_scores_FP_yscr.csv',sep=',',index=False)
+df_cv.to_csv('CAMDA-DILI/data/processed_data/Models/ECFP/cv_scores_ECFP.csv',sep=',',index=False)
 
 #export best params
-output = open('CAMDA-DILI/data/processed_data/Models/ECFP/best_params_FP_yscr.pkl','wb')
+output = open('CAMDA-DILI/data/processed_data/Models/ECFP/best_parameters_ECFP.pkl','wb')
 
 pickle.dump(best_params, output)
 
