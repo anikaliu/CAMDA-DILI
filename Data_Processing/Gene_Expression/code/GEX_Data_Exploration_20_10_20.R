5a14e02c9ccb05fe3a635cb510895fe91fa7336c
Anika Liu <anikaliu@yahoo.de>
2020-10-22 11:44:09 +0200

    Cleaned code for selecting landmark genes for conditions/compounds of interest

--- Data_Processing/Gene_Expression/code/GEX_Data_Exploration_20_10_20.R
+++ Data_Processing/Gene_Expression/code/GEX_Data_Exploration_20_10_20.R
@@ -56,58 +56,63 @@ GE_data_GC = GE_data_GC[GE_data_GC$pert_iname %in% Comp_GC$Compound.Name,] # get
 # https://stackoverflow.com/questions/45532058/subsetting-a-matrix-on-the-basis-of-a-list-of-some-of-the-column-names
 GE_data_GC_mat = as.data.frame(gct@mat) %>% select(one_of(GE_data_GC$id))
 
-# Get only landmark genes 
-gene_info <- read.csv("GSE70138_Broad_LINCS_gene_info_2017-03-06.txt", sep = '\t')
-gene_info <- gene_info[gene_info$pr_is_lm == 1,]
-
+# Get only landmark genes
 GE_data_GC_landmark_non_collapsed <- GE_data_GC_mat[rownames(GE_data_GC_mat) %in% gene_info$pr_gene_id, ] # Landmark genes only)
 
-
-# Find replicate columns - loop over do weighted sum, save into new column and replace old replicates
-
-modzs <- function(m) {
-  cc = cor(m, method="spearman") # pair-wise correlations
-  cc[cc<0] = 0
-  wt = 0.5*(colSums(cc)-1) # Per-sample values
-  wt[wt<0.01] = 0.01
-  sumWT = sum(wt)
-  normWT = wt / sumWT  # Normalized weights
-  # Return the scaled input values
-  return(m %*% normWT)
-}
-
-conditions<-GE_data_GC%>%select(cell_id,pert_time, pert_dose, pert_iname)%>%unique()
-df<-data.frame(matrix(NA, nrow=nrow(GE_data_GC_mat), ncol=nrow(conditions)))
-
-for (index in 1:nrow(conditions)){
-  print(index)
-  print(nrow(conditions))
-  cond<-conditions[index,]
-  id_interest<-GE_data_GC%>%right_join(cond) # should be GE_data_GC
-  id_col<-id_interest$id
-  replicates<-GE_data_GC_mat%>%select(id_col) # should be GE_data_GC_mat ####AL Does GE_data_GC_mat have a column called id_col? Either selecting here doesn't work or the conversion matrix below doesn't
-  
-  if (ncol(replicates)>1){
-    signature<-modzs(as.matrix(replicates))
-  }else{
-    signature<-replicates
-  }
-  colname<-paste0(cond[,1],'_', cond[,2],'_', cond[,3],'_', cond[,4])
-  df[,index]<-signature
-  colnames(df)[index]<-colname
-}
-
-# Collapsed to only landmark genes
-dftest = df
-rownames(dftest) = rownames(df$A375_6_10.0_alaproclate) ####AL column shouldn't have rownames
-dftest <- dftest[rownames(dftest) %in% gene_info$pr_gene_id, ] # Landmark genes only #########AL This might select the wrong rows
-aa = gene_info[match(rownames(dftest),gene_info$pr_gene_id ),2] # mapping to gene symbol
-
-rownames(dftest)= aa
-dftest$id = aa
-rownames(GE_data_GC_landmark_non_collapsed) = aa
-GE_data_GC_landmark_non_collapsed$id = aa
-
-
-write_csv("../data/L1000_Data_collapsed_all_conditionsv2_new.csv", x = dftest)
+#Map gene ID to gene symbols
+gene_info <- read.csv("GSE70138_Broad_LINCS_gene_info_2017-03-06.txt", sep = '\t')
+gene_info <- gene_info[gene_info$pr_is_lm == 1,]
+genesymbols = gene_info[match(rownames(GE_data_GC_landmark_non_collapsed),gene_info$pr_gene_id ),2] # mapping to gene symbol
+rownames(GE_data_GC_landmark_non_collapsed) = genesymbols
+GE_data_GC_landmark_non_collapsed$id = genesymbols
 write_csv("../data/CAMDA_Model_Data/L1000_Data_uncollapsed_all_conditionsv2_new.csv", x = GE_data_GC_landmark_non_collapsed)
+
+
+# # Find replicate columns - loop over do weighted sum, save into new column and replace old replicates
+# 
+# modzs <- function(m) {
+#   cc = cor(m, method="spearman") # pair-wise correlations
+#   cc[cc<0] = 0
+#   wt = 0.5*(colSums(cc)-1) # Per-sample values
+#   wt[wt<0.01] = 0.01
+#   sumWT = sum(wt)
+#   normWT = wt / sumWT  # Normalized weights
+#   # Return the scaled input values
+#   return(m %*% normWT)
+# }
+# 
+# conditions<-GE_data_GC%>%select(cell_id,pert_time, pert_dose, pert_iname)%>%unique()
+# df<-data.frame(matrix(NA, nrow=nrow(GE_data_GC_mat), ncol=nrow(conditions)))
+# 
+# for (index in 1:nrow(conditions)){
+#   print(index)
+#  # print(nrow(conditions))
+#   cond<-conditions[index,]
+#   id_interest<-GE_data_GC%>%right_join(cond) # should be GE_data_GC
+#   id_col<-id_interest$id
+#   replicates<-GE_data_GC_mat%>%select(all_of(id_col)) # should be GE_data_GC_mat 
+#   
+#   #If there are multiple replicates aggregate using modzs
+#   if (ncol(replicates)>1){
+#     signature<-modzs(as.matrix(replicates))
+#   }else{
+#     signature<-replicates
+#   }
+#   colname<-paste0(cond[,1],'_', cond[,2],'_', cond[,3],'_', cond[,4])
+#   df[,index]<-signature
+#   colnames(df)[index]<-colname
+# }
+# 
+# # Collapsed to only landmark genes
+# dftest = df
+# #rownames(dftest) = rownames(df$A375_6_10.0_alaproclate) ####AL column shouldn't have rownames
+# rownames(dftest) = rownames(GE_data_GC_mat)
+# dftest <- dftest[rownames(dftest) %in% gene_info$pr_gene_id, ] # Landmark genes only 
+# genesymbols = gene_info[match(rownames(dftest),gene_info$pr_gene_id ),2] # mapping to gene symbol
+# 
+# rownames(dftest)= genesymbols
+# dftest$id = genesymbols
+# 
+# 
+# 
+# write_csv("../data/L1000_Data_collapsed_all_conditionsv2_new.csv", x = dftest)
