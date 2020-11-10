ce9a9cb393a2b6ec8da9e8aa821ac65f7eb082f0
Anika Liu <anikaliu@yahoo.de>
2020-11-10 17:56:09 +0100

    Gene expression figure

--- Machine_Learning/code/summarize_performance_gex.R
+++ Machine_Learning/code/summarize_performance_gex.R
@@ -5,27 +5,28 @@ library(viridis)
 
 ####Aggregate individual performance files####
 cv<-read.csv('~/Downloads/OneDrive_1_10-11-2020/MCNC_cv_scores_GEX_yscr.csv')%>%
-  separate(splits, sep='\\.',
-           into=c('dataset','rest','csv','data',
+  separate(splits, sep='\\)\\.csv\\.', into=c('cell', 'modelinfo'))%>%
+  separate(cell, into=c('cell_line', 'time', 'dose'), sep=', ')%>%
+  separate(modelinfo, sep='\\.',
+           into=c('dataset',
                   'method', 'true_or_y_scrambled_labels_used',
                   'train_test_split','cv_split'))%>%
-  separate(dataset, into=c('cell_line', 'time', 'dose'), sep=', ')%>%
   mutate(cell_line=str_extract(pattern='[\\d\\w]+', cell_line))%>%
   mutate('testset'='LOCO-CV', 'descriptor'='L1000')
 ts<-read.csv('~/Downloads/OneDrive_1_10-11-2020/MCNC_ts_scores_GEX_yscr.csv')%>%
-  separate(splits, sep='\\.',
-           into=c('dataset','rest','csv','data', 'method', 
-                  'true_or_y_scrambled_labels_used',
+  separate(splits, sep='\\)\\.csv\\.', into=c('cell', 'modelinfo'))%>%
+  separate(cell, into=c('cell_line', 'time', 'dose'), sep=', ')%>%
+  separate(modelinfo, sep='\\.',
+           into=c('dataset',
+                  'method', 'true_or_y_scrambled_labels_used',
                   'train_test_split','cv_split'))%>%
-  separate(dataset, into=c('cell_line', 'time', 'dose'), sep=', ')%>%
   mutate(cell_line=str_extract(pattern='[\\d\\w]+', cell_line))%>%
   mutate('testset'='External Test Set', 'descriptor'='L1000')
 
 df_performance<-rbind(cv,ts)%>%
-  mutate(dataset='MCNC')%>%
   mutate(condition=ifelse(cell_line=='All_Cell_Lines','All cell lines',paste0(cell_line,' (',time,' h, ', dose,' uM)')))%>%
   mutate(condition=factor(condition, levels=unique(c('All cell lines', unique(condition)))))%>%
-  dplyr::select(condition, balanced_accuracy,dataset,testset,true_or_y_scrambled_labels_used)%>%
+  dplyr::select(condition, balanced_accuracy,dataset,testset,true_or_y_scrambled_labels_used,method)%>%
   mutate(balanced_accuracy=as.numeric(balanced_accuracy))
 
 df_performance$dataset_readable=NA
@@ -48,9 +49,13 @@ df_scrambledmedians<-df_performance%>%
 df_standardmeans<-df_performance%>%
   filter(true_or_y_scrambled_labels_used ==1)%>%
   group_by(condition, dataset, testset, dataset_readable)%>%summarise("median_balacc"=mean(balanced_accuracy, na.rm=T))%>%mutate('mean'="Mean performance")
+df_standardmedians<-df_performance%>%
+  filter(true_or_y_scrambled_labels_used ==1)%>%
+  group_by(condition, dataset, testset, dataset_readable)%>%summarise("median_balacc"=median(balanced_accuracy, na.rm=T))%>%mutate('mean'="Mean performance")
 
 
-gg_gex<- ggplot(data = df_models,aes(x=dataset_readable, y=as.numeric(balanced_accuracy ))) +
+
+gg_gex<- ggplot(data = df_models,aes(x=dataset, y=as.numeric(balanced_accuracy ))) +
     geom_boxplot(data = df_models, aes(fill=condition))+
     geom_point(data=df_scrambledmedians,
                aes(y=median_balacc,fill=condition, shape=scrambled, group=condition),
@@ -60,13 +65,13 @@ gg_gex<- ggplot(data = df_models,aes(x=dataset_readable, y=as.numeric(balanced_a
                position=position_dodge(width=0.75), size=2)+
     scale_shape_manual(values=c(23,24))+
     scale_fill_viridis_d(option='plasma', begin=0.3, end=1)+
-    facet_wrap(~testset) + theme_bw()+
+    facet_grid(method~testset) + theme_bw()+
     ylab("Balanced Accuracy")+
   guides(guide_legend(ncol=2))+
     theme(text=element_text(size=13), axis.text.x=element_text(angle=60, hjust=1),legend.title = element_blank(), axis.title.x = element_blank())
 
 
 
-ggsave(gg_gex,filename = '../plots/gex_performance.pdf', height = 180, width=300, units="mm")
-ggsave(gg_gex,filename = '../plots/gex_performance.jpeg', height = 180, width=300, units="mm")
+ggsave(gg_gex,filename = '../plots/gex_performance.pdf', height = 250, width=300, units="mm")
+ggsave(gg_gex,filename = '../plots/gex_performance.jpeg', height = 250, width=300, units="mm")
 
