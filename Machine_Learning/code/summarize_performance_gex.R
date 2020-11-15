9f6cf89495938d077e9f30a633f05308fcd22a40
Anika Liu <anikaliu@yahoo.de>
2020-11-15 17:02:24 +0100

    MCNC figure

--- Machine_Learning/code/summarize_performance_gex.R
+++ Machine_Learning/code/summarize_performance_gex.R
@@ -23,6 +23,25 @@ ts<-read.csv('~/Downloads/OneDrive_1_11-11-2020/MCNCts_scores_GEX_yscr.csv')%>%
   mutate(cell_line=str_extract(pattern='[\\d\\w]+', cell_line))%>%
   mutate('testset'='External Test Set', 'descriptor'='L1000')
 
+cv_mclcnc<-read.csv('~/Downloads/cv_Updated_MCLCNC_scores_GEX_yscr.csv')%>%
+  separate(splits, sep='\\.csv\\.', into=c('cell', 'modelinfo'))%>%
+  separate(cell, into=c('cell_line', 'time', 'dose'), sep=', ')%>%
+  separate(modelinfo, sep='\\.',
+           into=c('dataset',
+                  'method', 'true_or_y_scrambled_labels_used',
+                  'train_test_split','cv_split'))%>%
+  mutate(cell_line=str_extract(pattern='[\\d\\w]+', cell_line))%>%
+  mutate('testset'='LOCO-CV', 'descriptor'='L1000')
+ts_mclcnc<-read.csv('~/Downloads/OneDrive_1_12-11-2020/MCLCNC_ts_scores_GEX_yscr.csv')%>%
+  separate(splits, sep='\\.csv\\.', into=c('cell', 'modelinfo'))%>%
+  separate(cell, into=c('cell_line', 'time', 'dose'), sep=', ')%>%
+  separate(modelinfo, sep='\\.',
+           into=c('dataset',
+                  'method', 'true_or_y_scrambled_labels_used',
+                  'train_test_split','cv_split'))%>%
+  mutate(cell_line=str_extract(pattern='[\\d\\w]+', cell_line))%>%
+  mutate('testset'='External Test Set', 'descriptor'='L1000')
+
 df_performance<-rbind(cv,ts)%>%
   mutate(dose=gsub(dose,pattern='\\.0\\)', replacement = ''))%>%
   mutate(condition=ifelse(cell_line=='All_Cell_Lines','All cell lines',paste0(cell_line,' (',time,' h, ', dose,' uM)')))%>%
@@ -31,8 +50,7 @@ df_performance<-rbind(cv,ts)%>%
   mutate(balanced_accuracy=as.numeric(balanced_accuracy))
 
 df_performance$dataset_readable=NA
-df_performance$dataset_readable[which(df_performance$dataset=='all')]<-'DILIrank \n (+SIDER)'
-df_performance$dataset_readable[which(df_performance$dataset=='MCLCNC')]<-'DILIrank'
+df_performance$dataset_readable[which(df_performance$dataset=='all')]<-'DILIrank'
 df_performance$dataset_readable[which(df_performance$dataset=='MCNC')]<-'DILIrank \n (-vLessConcern)'
 #write.csv(df_performance,'../data/Model_Results_Parameters/summarized_performance.csv', row.names = F)
 
@@ -56,7 +74,7 @@ df_allmedians<-df_performance%>%
 
 write.csv(df_allmedians, '../data/LOOCV_Results/median_performance_gex.csv')
 
-gg_gex<- ggplot(data = df_models,aes(x=dataset, y=as.numeric(balanced_accuracy ))) +
+gg_gex<- ggplot(data = df_models,aes(x=testset, y=as.numeric(balanced_accuracy ))) +
     geom_boxplot(data = df_models, aes(fill=condition))+
     geom_point(data=df_scrambledmedians,
                aes(y=median_balacc,fill=condition, shape=scrambled, group=condition),
@@ -66,15 +84,17 @@ gg_gex<- ggplot(data = df_models,aes(x=dataset, y=as.numeric(balanced_accuracy )
                position=position_dodge(width=0.75), size=2)+
     scale_shape_manual(values=c(23,24))+
     scale_fill_viridis_d(option='plasma', begin=0.3, end=1)+
-    facet_grid(method~testset) + theme_bw()+
-    ylab("Balanced Accuracy")+
+    facet_wrap(~method) + theme_bw()+
+    ylab("Balanced accuracy")+
   guides(guide_legend(ncol=2))+
-    theme(text=element_text(size=13), axis.text.x=element_text(angle=60, hjust=1),legend.title = element_blank(), axis.title.x = element_blank())
+    theme(text=element_text(size=13),
+          #axis.text.x=element_text(angle=60, hjust=1),
+          legend.title = element_blank(), axis.title.x = element_blank())
 
 
 
-ggsave(gg_gex,filename = '../plots/gex_performance.pdf', height = 250, width=300, units="mm")
-ggsave(gg_gex,filename = '../plots/gex_performance.jpeg', height = 250, width=300, units="mm")
+ggsave(gg_gex,filename = '../plots/gex_performance_balanced_accuracy.pdf', height = 130, width=250, units="mm")
+ggsave(gg_gex,filename = '../plots/gex_performance_balanced_accuracy.jpeg',height = 130, width=250, units="mm")
 
 gg_gex<- ggplot(data = df_models,aes(x=condition, y=as.numeric(balanced_accuracy ))) +
   geom_boxplot(data = df_models, aes(fill=testset))+
